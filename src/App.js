49c11f7bc7e18af7ef4f25ed209bba9502ebd392
Aleksander <64606852+Alex-VGB@users.noreply.github.com>
2020-08-08 12:51:28 +0200

    quiz-app first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from "./hoc/Layout/Layout";
+
+function App() {
+    return (
+        <div className="App">
+            <Layout>
+
+
+
+            </Layout>
+        </div>
+    );
+}
+
+export default App;
