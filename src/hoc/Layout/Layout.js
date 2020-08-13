c0fb8511850bc9b31ef82668555845b097e9b94a
Aleksander <64606852+Alex-VGB@users.noreply.github.com>
2020-08-13 10:30:40 +0200

    menu implementation

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Layout.module.css'
 import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
+import Drawer from "../../components/Navigation/Drawer/Drawer";
 
 class Layout extends React.Component {
 
@@ -17,10 +18,12 @@ class Layout extends React.Component {
     render() {
         return (
             <div className={s.Layout}>
-
+                <Drawer
+                    isOpen={this.state.menu}
+                />
                 <MenuToggle
-                onToggle={this.toggleMenuHandler}
-                isOpen={this.state.menu}
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
                 />
                 <main>
                     {this.props.children}
