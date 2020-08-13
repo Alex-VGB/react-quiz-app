06e887ccd30fdbcbc709439fdf01dd37868a4ace
Aleksander <64606852+Alex-VGB@users.noreply.github.com>
2020-08-13 10:06:31 +0200

    menu switching

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import s from './Layout.module.css'
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
 
-class Layout extends React.Component{
+class Layout extends React.Component {
+
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
     render() {
         return (
             <div className={s.Layout}>
 
-
+                <MenuToggle
+                onToggle={this.toggleMenuHandler}
+                isOpen={this.state.menu}
+                />
                 <main>
                     {this.props.children}
                 </main>
